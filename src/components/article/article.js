34591c3f70dbfac593824e49bf34ae9d64660cf9
Oleh Sihunov <lancelot.lviv@gmail.com>
2021-01-13 17:46:41 +0200

    Article Component Basis

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Button from '../Button/button';
+import img from '../../img/img1.jpg'
 import './article.css'
-export default function Article(){
+export default function Article({date,title,text}){
     return(
-        <div className ="article">ArticleContainer</div>
+        <div className ="article">
+            <div className ="imageBlock"><img src={img}></img></div>
+            <div className ="dateBlock">24-10-2056</div>
+            <div className ="textBlock">
+                <div>
+                    <h2>Some Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                 </div>
+                <Button btntext="Find Same"></Button>
+            </div>
+        </div>
     )
 }
